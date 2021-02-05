3cfd9a8ba670e282e7f53a213ccb3fbfe03aa5b9
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-06 00:40:52 +0300

    Add project files.

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. sayi 5");
+            Console.WriteLine("2. sayi 2");
+            //Toplama islemi
+            Console.WriteLine("Toplama islemi sonucu:" + toplama(5, 2));
+            Console.WriteLine("Cıkarma islemi sonucu:" + cıkarma(5, 2));
+            Console.WriteLine("Bolme islemi sonucu:" + bolme(5, 2));
+            Console.WriteLine("Carpma islemi sonucu:" + carpma(5, 2));
+
+        }
+
+        //sayılar
+
+
+
+        //toplama kods
+        static int toplama(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+
+
+        }
+
+        //cıkarma kods
+
+        static int cıkarma(int sayi1, int sayi2)
+        {
+
+            return sayi1 - sayi2;
+
+
+
+        }
+
+
+
+
+
+
+
+        //carpma kods
+
+
+        static int carpma(int sayi1, int sayi2) {
+
+
+            return sayi1 * sayi2;
+        
+        
+        }
+
+
+
+
+
+        //bolme kods
+        static float bolme(float sayi1, float sayi2)
+        {
+
+            return sayi1 / sayi2;
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
